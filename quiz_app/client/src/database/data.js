9c9f7ccab6fa13a3fa6ec3b91d269aaaaebe27b9
Pragati Prasanna <pragatiprasanna@Pragatis-MBP.bbrouter>
2023-06-20 11:58:29 +0530

    commit

---
+++ quiz_app/client/src/database/data.js
@@ -0,0 +1,85 @@
+export default[
+    {
+     id:1,
+     question : "Mental illnesses are only hereditary, you cannot develop them on your own. True or false?",
+     options : [
+         'False',
+         'True',
+         
+     ]
+    },
+    {
+     id:2,
+     question : "Which one of these can be a common sign of mental health issues?",
+     options : [
+         'Problems Cocentrating',
+         'Healthy Eating Habits',
+     ]
+    },
+    {
+     id:3,
+     question : "Which one of these conditions can lead to (or show existing) mental health conditions?",
+     options : [
+         'Irregular Periods',
+         'Sleep Disorders',
+     ]
+    },
+    {
+     id:4,
+     question : "Therapy is only for people with serious mental health issues. True or false? ",
+     options : [
+         'True.Only people with serious problems should deek therapy',
+         'False.Anyone can go for therapy',
+     ]
+    },
+    {
+     id:5,
+     question : "Mental illness",
+     options : [
+         'Cannot be treated',
+         'Can be treated',
+     ]
+    },
+    {
+     id:6,
+     question : "Mental health is:",
+     options : [
+         'Only important for some people',
+         'An important part of overall health and well-being',
+     ]
+    },
+    {
+     id:7,
+     question : "If you know someone with poor mental health, you can help by:",
+     options : [
+         'Reaching out and letting them know help is available.',
+         'Ignoring them',
+     ]
+    },
+    {
+     id:8,
+     question : "People with mental illness are violent",
+     options : [
+         'True',
+         'False',
+     ]
+    },
+    {
+     id:9,
+     question : "Half of all mental illnesses occur before a person turns ______ years old.",
+     options : [
+         '14',
+         '18',
+     ]
+    },
+    {
+     id:10,
+     question : "Mental illness is caused by",
+     options : [
+         'Lack of willpower',
+         'A number of factors including biological factors,stressful or traumatic life events, and long lasting health conditions.',
+     ]
+    },
+ ];
+ 
+ export const answers = [0,0,1,1,1,1,0,1,0,1];
